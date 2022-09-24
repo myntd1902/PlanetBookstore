4c7fb02c36bcb512aa14c5e4c113e94637557480
myntd1902 <myntd1902@gmail.com>
2022-09-25 00:12:37 +0700

    Bootswatch & add Category to database

--- PlanetBook/PlanetBook.DataAccess/Data/ApplicationDbContext.cs
+++ PlanetBook/PlanetBook.DataAccess/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PlanetBook.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,6 @@ namespace PlanetBook.DataAccess.Data
             : base(options)
         {
         }
+        public DbSet<Category> Categories { get; set; }
     }
 }
---
+++ PlanetBook/PlanetBook.Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetBook.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Tên thể loại")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
