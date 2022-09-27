066ec3fa000b1b8518826fd50f330b4e5e4f76c8
myntd1902 <myntd1902@gmail.com>
2022-09-27 18:08:19 +0700

    Home Page

---
+++ PlanetBook/PlanetBookWeb/Areas/Customer/Controllers/HomePageController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PlanetBook.DataAccess.Repository.IRepository;
+using PlanetBook.Models;
+using PlanetBook.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlanetBookWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class HomePageController : Controller
+    {
+        private readonly ILogger<HomePageController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomePageController(ILogger<HomePageController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
+            return View(productList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
